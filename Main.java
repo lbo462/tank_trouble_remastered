2f865c265440dc16dd194fb80496b4744b217cf0
leoBonnaire <laithaut.lisser@gmail.com>
2022-03-04 16:53:18 +0100

    Rename and initial code

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+      JFrame window = new JFrame();
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window.setResizable(false);
+      window.setTitle("Tank trouble");
+
+      window.setLocationRelativeTo(null);
+      window.setVisible(true);
+
+    }
+}
--- TankTroubleRemastered.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Graphics2D;
-
-public class TankTroubleRemastered {
-    public static void main(String[] args) {
-        System.out.println("Ho ho");
-    }
-}
