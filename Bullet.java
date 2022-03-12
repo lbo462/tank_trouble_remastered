81b08f595d13d9cabdbf9d9942d97790e3699ed2
arosard <alexandre@rosard.info>
2022-03-12 18:47:22 +0100

    better looking code

--- Bullet.java
+++ Bullet.java
@@ -3,21 +3,22 @@ import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 
-public class Bullet extends Entity {
+public class Bullet extends MovingEntity {
 
   double bornAt;
   double lifeTime = 5000; // time to live in ms
   public boolean dead = false;
-  double direction;
 
   AffineTransform at = new AffineTransform();
+  private boolean UoDcollision;// Up or Down collisions
+  private boolean LoRCollision;// Left or Right collisions
 
   public Bullet(int x, int y, double direction,GamePanel gp) {
 
     this.x = x;
     this.y = y;
     this.gp = gp;
-    this.direction = direction;
+    this.angle = direction;
     this.speed = 4;
     bornAt = System.currentTimeMillis();
 
@@ -30,11 +31,24 @@ public class Bullet extends Entity {
 
   public void update() {
 
-    int nextX = x + (int)(speed * Math.sin(Math.toRadians(direction)));
-    int nextY = y + (int)(speed * Math.cos(Math.toRadians(direction)));
+    this.updateNextPosition();
+    this.collision();
+    this.didItTouch();
+    this.deadOrAlive();
+    this.updatePosition();
 
-    boolean LoRCollision = false, UoDcollision = false; // Left or Right / Up or Down collisions
+  }
 
+  public void draw(Graphics2D g2) {
+    g2.drawImage(sprite, x, y, null);
+  }
+
+  @Override
+  void collision() {
+    nextX += 5;
+    nextY += 5;
+    LoRCollision = false;
+    UoDcollision = false;
     // collision with window bounds
     if(nextY < gp.tileSize/2 || nextY >= gp.height - gp.tileSize/2) UoDcollision = true;
     else if(nextX < gp.tileSize/2 || nextX >= gp.width - gp.tileSize/2) LoRCollision = true;
@@ -45,61 +59,51 @@ public class Bullet extends Entity {
 
       if(gp.currentMap.tiles[yGrid][xGrid].collision) {
         Tile currentTile = gp.currentMap.tiles[yGrid][xGrid];
-        int xTile = (int)(xGrid * gp.tileSize);
-        int yTile = (int)(yGrid * gp.tileSize);
+        int xTile = (int)(xGrid * gp.tileSize) + gp.tileSize/2;
+        int yTile = (int)(yGrid * gp.tileSize) + gp.tileSize/2;
         int deltaX = nextX - xTile;
         int deltaY = nextY - yTile;
-        if(currentTile.up && Math.abs(deltaX) < 5 && deltaY < 0)
-          LoRCollision = true;
-        else if(currentTile.down && Math.abs(deltaX) < 5 && deltaY > 0)
-          LoRCollision = true;
-        else if(currentTile.right && Math.abs(deltaY) < 5 && deltaX > 0)
-          UoDcollision = true;
-        else if(currentTile.left && Math.abs(deltaY) < 5 && deltaX < 0)
-          UoDcollision = true;
+
+        // Magic piece of code, do not touch
+        if(deltaX > 0)
+          deltaX -= 5;
+        if(deltaY > 0)
+          deltaY -= 5;
+
+        if(Math.abs(deltaX) < 5 && deltaY < 0) { // coming from top
+          if(currentTile.up) LoRCollision = true;
+          else if(currentTile.down) UoDcollision = true;
+        }
+        if(Math.abs(deltaX) < 5 && deltaY > 0) { // coming from bottom
+          if(currentTile.down) LoRCollision = true;
+          else if(currentTile.up) UoDcollision = true;
+        }
+        if(Math.abs(deltaY) < 5 && deltaX > 0) { //coming from right
+          if(currentTile.right) UoDcollision = true;
+          else if(currentTile.left) LoRCollision = true;
+        }
+        if(Math.abs(deltaY) < 5 && deltaX < 0) { //coming from left
+          if(currentTile.left) UoDcollision = true;
+          else if(currentTile.right) LoRCollision = true;
+        }
+
+
       }
 
     }
-
-/*
-    if(nextY < 0 || yGrid >= gp.currentMap.tiles.length || nextX < 0 || xGrid >= gp.currentMap.tiles[yGrid].length) {
-      if(nextY < 0 || yGrid >= gp.currentMap.tiles.length) UoDcollision = true;
-      else if(nextX < 0 || xGrid >= gp.currentMap.tiles[yGrid].length) LoRCollision = true;
+    if(UoDcollision) {
+      // left or right
+      // find the angle with same cos and opposite sin
+      angle = 180 - angle;
+    } else if(LoRCollision) {
+      // up or down
+      // find the angle with same sin and oppsite cos
+      angle *= -1;
     }
-    else if(gp.currentMap.tiles[yGrid][xGrid].collision) {
-      Tile currentTile = gp.currentMap.tiles[yGrid][xGrid];
-      int xTile = (int)(xGrid * gp.tileSize + gp.tileSize/2);
-      int yTile = (int)(yGrid * gp.tileSize + gp.tileSize/2);
-      int deltaX = x+5 - xTile;
-      int deltaY = y+5 - yTile;
-      if(currentTile.up && Math.abs(deltaX) < gp.tileSize/2 && deltaY < 0)
-        LoRCollision = true;
-      else if(currentTile.down && Math.abs(deltaX) < gp.tileSize/2 && deltaY > 0)
-        LoRCollision = true;
-      else if(currentTile.right && Math.abs(deltaY) < gp.tileSize/2 && deltaX > 0)
-        UoDcollision = true;
-      else if(currentTile.left && Math.abs(deltaY) < gp.tileSize/2 && deltaX < 0)
-        UoDcollision = true;
-    }
-*/
-
-    if(LoRCollision || UoDcollision) {
-      if(UoDcollision) {
-        // left or right
-        // find the angle with same cos and opposite sin
-        direction = 180 - direction;
-      } else if(LoRCollision) {
-        // up or down
-        // find the angle with same sin and oppsite cos
-        direction *= -1;
-      }
-    }
-    x += speed * Math.sin(Math.toRadians(direction));
-    y += speed * Math.cos(Math.toRadians(direction));
-
+  }
+  // wait few ms and check if a player was hit
+  public void didItTouch(){
     double currentTime = System.currentTimeMillis();
-
-    // wait few ms and check if a player was hit
     if(currentTime - bornAt > 500) { // don't auto kill the shooter
       for(Tank t: gp.players) {
 
@@ -115,12 +119,17 @@ public class Bullet extends Entity {
         }
       }
     }
-    // check it is time to die
+  }
+
+  // check it is time to die
+  public void deadOrAlive(){
+    double currentTime = System.currentTimeMillis();
     if(currentTime - bornAt > lifeTime) dead = true;
-
   }
 
-  public void draw(Graphics2D g2) {
-    g2.drawImage(sprite, x, y, null);
+  public void updatePosition(){
+    x += speed * Math.sin(Math.toRadians(angle));
+    y += speed * Math.cos(Math.toRadians(angle));
   }
+
 }
