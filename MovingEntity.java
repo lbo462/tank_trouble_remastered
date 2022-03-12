2896bd7727ae3eeabab715e24b912739260762b7
arosard <alexandre@rosard.info>
2022-03-12 23:53:37 +0100

    base class of moving objects

---
+++ MovingEntity.java
@@ -0,0 +1,15 @@
+public abstract class MovingEntity extends Entity{
+    int nextX,nextY;
+    double angle;
+    double speed;
+
+
+    public void updateNextPosition(){
+        nextX = x + (int)(speed * Math.sin(Math.toRadians(angle)));
+        nextY = y + (int)(speed * Math.cos(Math.toRadians(angle)));
+    }
+    
+    //abstract void update(); is not specified as it is an abstract class (i.e. supports abstract methods)
+    abstract void collision();
+    abstract void updatePosition();
+}
