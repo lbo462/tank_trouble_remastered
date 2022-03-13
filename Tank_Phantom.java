270e8f00dc8c2549cba4073036f0d6f61e9ddff0
AdriMerle <adrinoljeu@gmail.com>
2022-03-13 19:59:30 +0100

    Added comments, renamed SuperTank to Tank_Super (such that all tanks are side to side in Win Explorer)

---
+++ Tank_Phantom.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics2D;
+import java.awt.AlphaComposite;
+
+public class Tank_Phantom extends Tank_Super{
+    public Tank_Phantom(int number, int x, int y, String image, GamePanel gp, KeyHandler keyH){
+        super(number, x, y, image, gp, keyH,3000,20000);
+    }
+
+    public void update(){
+        this.keyPressed();
+        this.capacityActivation();
+        if(this.capacityActivated)
+            collisionWithTiles = false;
+        else
+            collisionWithTiles = true;
+        super.update();
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+      // transparency
+      AlphaComposite alcom;
+      if(this.capacityActivated)
+        alcom = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
+      else
+        alcom = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f);
+      g2.setComposite(alcom);
+
+      super.draw(g2);
+      alcom = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f);
+      g2.setComposite(alcom);
+    }
+}
