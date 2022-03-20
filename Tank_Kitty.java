88e37e680793e55483de43bcb8be0e65c8ceabb3
leoBonnaire <leo.bonnaire@insa-lyon.fr>
2022-03-20 16:37:38 +0100

    bullet counter for kitty

--- Tank_Kitty.java
+++ Tank_Kitty.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.awt.Graphics2D;
+import java.awt.Color;
 
 // Super funny tank that has a "super" capacity : create slow zone
 public class Tank_Kitty extends Tank_Super{
@@ -13,17 +14,18 @@ public class Tank_Kitty extends Tank_Super{
     @Override
     public void update(){
         super.update();
-        if(this.capacityActive)
+        if(this.capacityActive && bulletsKitty.size() < 5)
             shoot();
     }
 
     @Override
     public void deadBulletRemoval(){
       super.deadBulletRemoval();
-      for(int i = 0; i < bulletsKitty.size(); i++) {
+
+      for(int i = bulletsKitty.size()-1; i >= 0; i--) {
         Bullet_Kitty current = bulletsKitty.get(i);
         current.update();
-        if(current.dead || (bulletsKitty.size() > 5 && current.exploded)){
+        if(current.dead || (bulletsKitty.size() > 5 && !current.exploded)){
           bulletsKitty.remove(i);
         }
       }
@@ -44,6 +46,17 @@ public class Tank_Kitty extends Tank_Super{
     @Override
     public void draw(Graphics2D g2) {
       for(int i = 0; i < bulletsKitty.size(); i++) bulletsKitty.get(i).draw(g2);
+
+      // draw number of bullets_kitty remaining
+      g2.setColor(Color.PINK);
+      int rectWidth = gp.tileSize / 5;
+      for(int i = 0; i < 5-bulletsKitty.size(); i ++) {
+        g2.setColor(Color.PINK);
+        g2.fillRect(getX()-width/2+i*rectWidth, getY()-height, rectWidth-2, height / 4);
+        g2.setColor(Color.BLACK);
+        g2.drawRect(getX()-width/2+i*rectWidth, getY()-height, rectWidth-2, height / 4);
+      }
+
       super.draw(g2);
     }
 }
