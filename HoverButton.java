ccb5ddd1b74cad0f2c0e8a07332ddf30222e54c7
arosard <alexandre@rosard.info>
2022-04-15 14:41:09 +0200

    adding sounds for the button click (not working yet)

--- HoverButton.java
+++ HoverButton.java
@@ -18,10 +18,19 @@ public class HoverButton extends JButton implements MouseListener{
         addMouseListener(this);
         click = new AudioClip();
         URL url;
+        url = getClass().getResource("assets/sounds/click.wav");
+        click = Applet.newAudioClip(url);
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) {
+        try{
+            click.start();
+        }catch(Exception e){
+            System.out.println("problem with HoverButton click sound");
+            e.printStackTrace();
+        }
+     }
 
     @Override
     public void mousePressed(MouseEvent e) {}
