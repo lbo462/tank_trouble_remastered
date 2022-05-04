aee6851fb08919508f33cafff660c9dde16baffb
leoBonnaire <leo.bonnaire@insa-lyon.fr>
2022-05-04 22:50:50 +0200

    few comments

--- PUSpeedUp.java
+++ PUSpeedUp.java
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 
 // Power up that add the SpeedBoost effect to the player (so he's faster)
-=======
->>>>>>> 09aa4aede69fc6280c09466c78f7a7c7c6706e7a
 public class PUSpeedUp extends PowerUp{
 
     public PUSpeedUp(GamePanel gPanel, int xCoord, int yCoord) {
--- PathFindingNode.java
+++ PathFindingNode.java
@@ -1,12 +1,8 @@
 import java.util.ArrayList;
 
 
-<<<<<<< HEAD
 // used for BFS, defines a node inside the queue
 // not used on this version, not enough time to finish the implementation of Djikstra
-=======
-
->>>>>>> 09aa4aede69fc6280c09466c78f7a7c7c6706e7a
 public class PathFindingNode {
   public int id;
   public ArrayList<Integer> neighbours = new ArrayList<Integer>();
